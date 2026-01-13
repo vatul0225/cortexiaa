adec88e1358121ca2e01b54676846b4b698b4a4d
vatul0225 <atulverma0225@gmail.com>
2026-01-13 14:34:15 +0530

    Update Discussion.js

--- Backend/models/Discussion.js
+++ Backend/models/Discussion.js
@@ -1,3 +1,25 @@
+// import mongoose from "mongoose";
+
+// const DiscussionSchema = new mongoose.Schema(
+//   {
+//     fullName: { type: String, required: true },
+//     email: { type: String, required: true },
+//     phone: { type: String, required: true },
+//     company: String,
+//     projectType: String,
+//     budget: { type: String, required: true },
+//     date: { type: String, required: true },
+//     time: { type: String, required: true },
+//     services: { type: [String], required: true },
+//     message: String,
+//   },
+//   { timestamps: true }
+// );
+
+// export default mongoose.models.Discussion ||
+//   mongoose.model("Discussion", DiscussionSchema);
+
+
 import mongoose from "mongoose";
 
 const DiscussionSchema = new mongoose.Schema(
@@ -18,3 +40,4 @@ const DiscussionSchema = new mongoose.Schema(
 
 export default mongoose.models.Discussion ||
   mongoose.model("Discussion", DiscussionSchema);
+
